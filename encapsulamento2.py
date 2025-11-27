76ecb663a921298145b2b55c9ece9fee9e26d4ba
ezequielbrandao99 <ezequielbrandao001@gmail.com>
2025-11-27 10:56:39 -0300

    codigo para corrigir

--- encapsulamento2.py
+++ encapsulamento2.py
@@ -1,1 +1,21 @@
-class 
+class perfil:
+
+    def __init__(self, email, senha):
+        self.__email = email
+        self.__senha = senha
+
+    @property
+    def email(self):
+        return self.__email
+    
+    email.setter
+    def email(self, novo_email = None):
+
+        self.email = email
+        if novo_email is None:
+            email = str(input("Digite o novo email: "))
+
+            print('Email cadastrado: ', email)
+
+perfil.email = perfil
+print('Email cadastrado: ', perfil.email)
